7a35b1f66b09e6ced037a68c5cf333865bb84ee9
Dmitry Pokhmelnov <dmitry.pokhmelnov@akvelon.com>
2019-12-28 12:53:22 +0300

    fix

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+const slider = document.getElementById("myRange");
+const output = document.getElementById("demo");
+const submit = document.getElementById("submit");
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
+submit.onclick = function() {
+  alert(slider.value);
 }
